7662942af16e00edffe37455ac62733b7198ac30
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-01-22 19:45:46 +0700

    đang fix FE

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <header className="header">
-      <h1>Garden CMS</h1>
+      <h1>GREEN GARDEN SHOWCASE</h1>
     </header>
   );
 }
